4c6856990e839ab8e31345fa7d4b4cb74137ac2b
shivjoshi1996 <joshi.k.shiv@gmail.com>
2021-07-25 10:13:47 -0700

    Finish 1st and 2nd challenge

--- add/test.ts
+++ add/test.ts
@@ -1,16 +1,14 @@
-import { createDebuggerStatement } from "typescript";
-
 export function add(param1: number, param2: number): number {
   return param1 + param2;
 }
 
 export function add2(...param1: number[]): number {
-  return param1.reduce((total, current) => total + current);
+
+  console.log(param1.length);
+
 }
-
-
 // console.log(add(1, 2));
 // console.log(add(3, 2));
 
-console.log(add2(1,2,3,4,5));
-console.log(add2(2,3));
+// console.log(add2(1,2,3,4,5));
+// console.log(add2(2,3));
--- addBorder/addBorder.ts
+++ addBorder/addBorder.ts
@@ -1,5 +1,10 @@
 export function addBorder(picture: string[]): string[] {
-
+  const starredArray = picture.map(string => `*${string}*`);
+  const stringLength = picture[0].length;
+  const stars = '*'.repeat(stringLength + 2);
+  starredArray.push(stars);
+  starredArray.unshift(stars);
+  return starredArray;
 }
 
-// console.log(addBorder(["abc", "ded"]));
+console.log(addBorder(["abc", "ded"]));
